aaf772c0b974833c408d4521491a73776a3fdc76
Sayani07 <Sayani.Gupta@monash.edu>
2018-09-28 11:21:10 +1000

    Database created and data reprocessed -
    There were missing timestamps for some living units. To make it uniform, now this dataset has same timestamps for all living units and consumption are NA for those timestamps for which consumption unit not available. Now these table can be used to visualize days for which consumption is missing

--- R/create_database.R
+++ R/create_database.R
@@ -1,8 +1,53 @@
+rm(list=ls())
+
 library(RSQLite)
 library(dplyr)
 
-df <- read_rds("DHResidence.rds")
+data_file <- "NRAS Treatment Group Data Export_2018-05-31-15-57-41.csv"
+
+df <- read_csv(data_file,
+               col_types = cols(
+                 `Timestamp UTC` = col_datetime(format = ""),
+                 Timestamp = col_datetime(format = ""),
+                 Value = col_double(),
+                 Source = col_character(),
+                 Measurement = col_character(),
+                 Unit = col_character()
+               )) %>%
+  rename(
+    time.utc = `Timestamp UTC`,
+    time.local = `Timestamp`,
+    source = Source,
+    measurement = Measurement,
+    unit = Unit,
+    value = Value
+  ) %>%
+  mutate(
+    time.utc = as.numeric(strftime(time.utc, '%Y%m%d%H%M')),
+    time.local = as.numeric(strftime(time.local, '%Y%m%d%H%M')),
+    source = factor(source)
+  ) 
+
+date.range <- range(df$time.utc)
+date.range.POSIXct <- strptime(as.character(date.range), '%Y%m%d%H%M')
+
+reference.times <- data_frame(
+  time.utc = seq(
+    from=as.POSIXct(date.range.POSIXct[1], tz='UTC'),
+    to=as.POSIXct(date.range.POSIXct[2], tz='UTC'),
+    by='15 min'
+  )) %>%
+  mutate(
+    timeid = as.numeric(strftime(time.utc, '%Y%m%d%H%M')),
+    dateid = as.numeric(strftime(time.utc, '%Y%m%d')),
+    dayname = strftime(time.utc, '%A')
+  )
+
+df.uniform <- expand.grid(time=reference.times$timeid,
+                        source=unique(df$source)) %>%
+  left_join(df, by=c('time'='time.utc', 'source'))
 
 con <- dbConnect(RSQLite::SQLite(), "DHResidence.db")
-copy_to(con, df, name='energy', overwrite=TRUE, temporary = FALSE)
+copy_to(con, df.uniform, name='energy', overwrite=TRUE, temporary = FALSE)
 dbDisconnect(con)
+
--- R/reprocess_table.R
+++ R/reprocess_table.R
@@ -2,22 +2,16 @@ rm(list=ls())
 
 library(RSQLite)
 library(dplyr)
-library(dbplyr)
-library(replyr)
+# library(dbplyr)
+# library(replyr)
 
 con <- dbConnect(RSQLite::SQLite(), "DHResidence.db")
 energy <- tbl(con, 'energy')
 
-devtools::install_github("tidyverse/dbplyr", ref = devtools::github_pull(72))
+b105 <- energy %>%
+  filter(source == 'B1 05') %>%
+  collect()
 
-energy_spread <- energy %>% select(Timestamp,Source,Value) %>% 
-  spread(key=Timestamp,value=Value)
- 
-energy_gather <- energy_spread %>%
-  gather(Timestamp,Demand,-Source)
-
-
-
-energy_gather <- energy_spread %>%
-  build_pivot_control(columnToTakeKeysFrom= 'TimeStamp_All', 
-                      columnToTakeValuesFrom= 'Demand')
+rec.by.source <- energy %>%
+  group_by(source) %>%
+  summarize(count = n())
